c03e5e7267c89fd7dbc7231e88ec44fbd6499d35
James Cuff <95388561+DrCuff@users.noreply.github.com>
2025-08-31 23:07:54 -0400

    Still needs work.  But this is for Ian

--- pumpupthething.js
+++ pumpupthething.js
@@ -4,38 +4,38 @@
   @details strudel demo
 */
 
-let cpm = 135/4;
+let cpm = 120/4;
 const pickRestart = register('pickRestart', 
       (arr, pat) => pat.pick(arr.map((x)=>x.restart(pat.collect().fmap(v=>v+1)))))
 
 let synth_arpeggio = cat(
-  "<c3 c4 eb5 c3 c4 d5 c3 bb4>*8",
-  "<g2 g3 bb4 g2 g3 a4 g2 g4>*8",
-  "<eb2 eb3 g4 eb2 eb3 f4 eb2 g4>*8",
-  "<eb2 eb3 g4 eb2 eb3 f4 f2 g4>*8",
-  "<c3 c4 eb5 c3 c4 d5 c3 bb4>*8",
-  "<g2 g3 bb4 g2 bb4 c5 bb2 g4>*8",
-  "<eb2 eb3 g4 eb2 eb3 f4 eb2 g4>*8",
-  "<f2 f3 g4 f2 f3 a4 f2 a4>*8",
+  "<c3 c4 eb5 c3 c4 d5 c3 bb4>*16",
+  "<g2 g3 bb4 g2 g3 a4 g2 g4>*16",
+  "<eb2 eb3 g4 eb2 eb3 f4 eb2 g4>*16",
+  "<eb2 eb3 g4 eb2 eb3 f4 f2 g4>*16",
+  "<c3 c4 eb5 c3 c4 d5 c3 bb4>*16",
+  "<g2 g3 bb4 g2 bb4 c5 bb2 g4>*16",
+  "<eb2 eb3 g4 eb2 eb3 f4 eb2 g4>*16",
+  "<f2 f3 g4 f2 f3 a4 f2 a4>*16",
 ).note()
-  .n(1).sound("gm_pad_poly")
+  .n(1).sound("sawtooth")
   .decay(.95).lpf(5000).lpenv(-3).lpa(.2)
-  .delay(".3:.225:.45")
+  .delay(".3:.225:y.45")
   .room(.8).rsize(2);
 
 let synth_lead = cat(
-  "<- - eb5 - - d5 - bb4>*8",
-  "<- - bb4 - - a4 - g4>*8",
-  "<- - g4 - - f4 - g4>*8",
-  "<- - g4 - - f4 - g4>*8",
-  "<- - eb5 - - d5 - bb4>*8",
-  "<- - bb4 - bb4 c5 - g4>*8",
-  "<- - g4 - - f4 - g4>*8",
-  "<- - g4 - - a4 - a4>*8",
+  "<- - eb5 - - d5 - bb4>*16",
+  "<- - bb4 - - a4 - g4>*16",
+  "<- - g4 - - f4 - g4>*16",
+  "<- - g4 - - f4 - g4>*16",
+  "<- - eb5 - - d5 - bb4>*16",
+  "<- - bb4 - bb4 c5 - g4>*16",
+  "<- - g4 - - f4 - g4>*16",
+  "<- - g4 - - a4 - a4>*16",
 ).note()
   .n(1).sound("gm_pad_metallic")
   .decay(.5).delay(".3:.225:.45")
-  .room(.4).rsize(1).gain(.6);
+  .room(.4).rsize(10).gain(.6);
 
 // pumpupthejambeat
 let putj = cat("<0@4 [0,1]@12 [0,1,2]@4 [0,1,2,3]@4>".pickRestart(
@@ -48,11 +48,13 @@ let putj = cat("<0@4 [0,1]@12 [0,1,2]@4 [0,1,2,3]@4>".pickRestart(
  ,s("bd").gain(0.8)
  ]).bank("RolandTR909").color("yellow").velocity(0.7))
 
-let intro = stack(putj, synth_lead)
-let v1 = stack(putj, synth_arpeggio)
+let intro = stack(putj,synth_arpeggio)
+let v1 = stack(putj, synth_lead)
 
 arrange (
+  [2, putj],
   [4, intro],
+  [2, synth_arpeggio],
   [4, v1],
 ).cpm(cpm);
 
