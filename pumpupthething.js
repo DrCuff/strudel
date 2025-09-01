9458ae2f6075dae427a4b1052b534f859cab44a1
James Cuff <95388561+DrCuff@users.noreply.github.com>
2025-08-31 23:30:13 -0400

    piano pumpupthething.js

--- pumpupthething.js
+++ pumpupthething.js
@@ -1,3 +1,4 @@
+
 /*
   @title Repump the Jam v1.0
   @by ChromaMagic
@@ -23,20 +24,22 @@ let synth_arpeggio = cat(
   .delay(".3:.225:y.45")
   .room(.8).rsize(2);
 
-let synth_lead = cat(
-  "<- - eb5 - - d5 - bb4>*16",
-  "<- - bb4 - - a4 - g4>*16",
-  "<- - g4 - - f4 - g4>*16",
-  "<- - g4 - - f4 - g4>*16",
-  "<- - eb5 - - d5 - bb4>*16",
-  "<- - bb4 - bb4 c5 - g4>*16",
-  "<- - g4 - - f4 - g4>*16",
-  "<- - g4 - - a4 - a4>*16",
-).note()
-  .n(1).sound("gm_pad_metallic")
-  .decay(.5).delay(".3:.225:.45")
-  .room(.4).rsize(10).gain(.6);
 
+let piano = cat(
+  "<c3 c4 eb5 c3 c4 d5 c3 bb4>*8",
+  "<g2 g3 bb4 g2 g3 a4 g2 g4>*8",
+  "<eb2 eb3 g4 eb2 eb3 f4 eb2 g4>*8",
+  "<eb2 eb3 g4 eb2 eb3 f4 f2 g4>*8",
+  "<c3 c4 eb5 c3 c4 d5 c3 bb4>*8",
+  "<g2 g3 bb4 g2 bb4 c5 bb2 g4>*8",
+  "<eb2 eb3 g4 eb2 eb3 f4 eb2 g4>*8",
+  "<f2 f3 g4 f2 f3 a4 f2 a4>*8",
+).note()
+  .n(1).sound("piano")
+  .decay(.95).lpf(5000).lpenv(-3).lpa(.2)
+  .delay(".3:.225:y.45")
+  .room(.8).rsize(1);
+ 
 // pumpupthejambeat
 let putj = cat("<0@4 [0,1]@12 [0,1,2]@4 [0,1,2,3]@4>".pickRestart(
  [stack(s("oh*16").pan(0.45).gain("[0.08 0.16]*4").release(0),s("hh*4").pan(0.7).gain(0.20))
@@ -49,13 +52,14 @@ let putj = cat("<0@4 [0,1]@12 [0,1,2]@4 [0,1,2,3]@4>".pickRestart(
  ]).bank("RolandTR909").color("yellow").velocity(0.7))
 
 let intro = stack(putj,synth_arpeggio)
-let v1 = stack(putj, synth_lead)
+let v1 = stack(putj, synth_arpeggio, piano)
+let v2 = stack(piano)
 
 arrange (
   [2, putj],
-  [4, intro],
-  [2, synth_arpeggio],
-  [4, v1],
+  [8, intro],
+  [8, v1],
+  [4, v2],
 ).cpm(cpm);
 
 // @version 1.0
